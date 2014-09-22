107a29a0c06d7fcf68ce95ca4093503e61919ecb
Chris <wmfrancia@gmail.com>
2014-09-22 11:43:37 -0400

    Create splashup.php

---
+++ splashup.php
@@ -0,0 +1,116 @@
+<html>
+    <head>
+        <title>Your Content is Being Prepared</title>
+        <style>
+            body {
+                padding:0;
+                margin:0;
+            }
+            .header {
+                height:90px;
+                width:100%;
+                background: #6AC4F7;
+                
+            }
+            .header_img {
+                margin-left:17px;
+            }
+            .content {
+                
+            }
+            .ads {
+                width:680px;
+                margin:0 auto;
+            }
+            .left {
+                float:left;
+            }
+            .right {
+                float:right;
+            }
+            .count {
+                    -moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+                    -webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+                    box-shadow:inset 0px 1px 0px 0px #ffffff;
+                    background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #f9f9f9), color-stop(1, #e9e9e9) );
+                    background:-moz-linear-gradient( center top, #f9f9f9 5%, #e9e9e9 100% );
+                    filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#f9f9f9', endColorstr='#e9e9e9');
+                    background-color:#f9f9f9;
+                    -webkit-border-top-left-radius:20px;
+                    -moz-border-radius-topleft:20px;
+                    border-top-left-radius:20px;
+                    -webkit-border-top-right-radius:20px;
+                    -moz-border-radius-topright:20px;
+                    border-top-right-radius:20px;
+                    -webkit-border-bottom-right-radius:20px;
+                    -moz-border-radius-bottomright:20px;
+                    border-bottom-right-radius:20px;
+                    -webkit-border-bottom-left-radius:20px;
+                    -moz-border-radius-bottomleft:20px;
+                    border-bottom-left-radius:20px;
+                    text-indent:0;
+                    border:1px solid #dcdcdc;
+                    display:inline-block;
+                    color:#666666;
+                    font-family:Arial;
+                    font-size:15px;
+                    font-weight:bold;
+                    font-style:normal;
+                    height:40px;
+                    margin:17px;
+                    line-height: 0px;
+                    padding:25px;
+                    width:auto;
+                    text-decoration:none;
+                    text-align:center;
+                    text-shadow:1px 1px 0px #ffffff;
+            }
+            .count:hover {
+                    background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #e9e9e9), color-stop(1, #f9f9f9) );
+                    background:-moz-linear-gradient( center top, #e9e9e9 5%, #f9f9f9 100% );
+                    filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#e9e9e9', endColorstr='#f9f9f9');
+                    background-color:#e9e9e9;
+            }
+            .count:active {
+                    position:relative;
+                    top:1px;
+        </style>
+        <script src='./splashup.js'></script>
+    </head>
+    <body>
+        <div class='header'>
+            <div class='left'>
+                <img src="http://thegamingunderground.com/images/TGU_new_logo.png" width="288" height="75" alt="The Gaming Underground" class='header_img'>
+            </div>
+            <div class='right'>
+                <button class='count' id='count'>Loading</button>
+            </div>
+        </div>
+        <div class='content'>            
+            <div class='ads'>
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- Ad Waiting -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:336px;height:280px;margin-top: 120px;"
+                     data-ad-client="ca-pub-6919595555799258"
+                     data-ad-slot="6738462121"></ins>
+                <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- Ad Waiting -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:336px;height:280px;margin-top: 120px;"
+                     data-ad-client="ca-pub-6919595555799258"
+                     data-ad-slot="6738462121"></ins>
+                <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
+        </div>
+        <footer>
+            <script>countdown(10,'<?php echo $_POST['link']; ?>');</script>
+        </footer>
+    </body>
+    
+</html>
